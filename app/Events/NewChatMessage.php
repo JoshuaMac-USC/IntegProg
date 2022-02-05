c0ab398a9cb63c1af4fcddc43b9c5413a24f99f7
ChrisD921 <cdrape921@gmail.com>
2022-02-05 21:55:18 +0800

    Trying the Sockets #1

--- app/Events/NewChatMessage.php
+++ app/Events/NewChatMessage.php
@@ -15,6 +15,7 @@ class NewChatMessage implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $chatMessage;
     /**
      * Create a new event instance.
      *
@@ -34,4 +35,8 @@ class NewChatMessage implements ShouldBroadcast
     {
         return new PrivateChannel('chat.'. $this->chatMessage->chat_room_id );
     }
+
+    public function broadcastAs() {
+        return ‘message.new’;
+   }
 }
